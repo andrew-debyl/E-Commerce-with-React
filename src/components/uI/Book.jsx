376f8f083f2a5fd33cebc757e8318f9701dfa493
Andrew <andrewdebyl@gmail.com>
2023-03-29 14:53:54 -0400

    changes

--- src/components/uI/Book.jsx
+++ src/components/uI/Book.jsx
@@ -7,23 +7,21 @@ const Book = ({ book }) => {
   const [img, setImg] = useState();
 
   // When we switch routes dont set image to unmounted component
-  const mountedRef = useRef(false);
+  const mountedRef = useRef(true);
 
   useEffect(() => {
     const image = new Image();
     image.src = book.url;
     image.onload = () => {
       setTimeout(() => {
-        console.log("here 1")
-        if (mountedRef.current) {
+        if (!mountedRef.current) {
           setImg(image);
         }
       }, 300);
     };
     return () => {
       // When the component unmounts
-      console.log("here 2")
-      mountedRef.current = true;
+      mountedRef.current = false;
     };
   }, [book.url]);
 
